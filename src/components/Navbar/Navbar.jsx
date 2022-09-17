452e190e5f7e3c283bf8f303a20bb8e4151bd146
Joaopster <jpster@pop-os.localdomain>
2022-09-17 15:34:45 -0300

    Adicionado o Componente da Navbar + classes css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
+import { images } from '../../constants'
+
 import './Navbar.scss'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {['Inicio', 'Sobre', 'Trabalho', 'Skills', 'Testemunhos', 'Contato'].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item.toLowerCase()}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
