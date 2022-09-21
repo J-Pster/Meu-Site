a3c7a1b44cfad733b9ec0f2763367965cbedc850
Joaopster <jpster@pop-os.localdomain>
2022-09-21 17:18:16 -0300

    Corrigido erro de escrita no email

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -42,7 +42,7 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card">
           <img src={images.email} alt="email" />
-          <a href="mailto:joaopsterdev@gmail.com" className="p-text">joaopsterdev@gmail</a>
+          <a href="mailto:joaopsterdev@gmail.com" className="p-text">joaopsterdev@gmail.com</a>
         </div>
         <div className="app__footer-card">
           <img src={images.mobile} alt="mobile" />
